87b9c26ccde60df59bd33c987610f276fea6dee2
Thomas Levine <_@thomaslevine.com>
2015-04-04 15:23:26 +0000

    revert

---
+++ wikidump/reversion.py
@@ -0,0 +1,9 @@
+def is_reversion(comment):
+    '''
+    Parse a comment, and determine whether it is a reversion.
+    '''
+
+def account(comment):
+    '''
+    Determine the account whose edits were reverted.
+    '''
