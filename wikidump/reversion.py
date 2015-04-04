9b8c7d1a0d16a1d23db9162f11e6a5daf1b1b254
Thomas Levine <_@thomaslevine.com>
2015-04-04 15:46:10 +0000

    writing tests

--- wikidump/reversion.py
+++ wikidump/reversion.py
@@ -1,9 +1,19 @@
+import re
+
 def is_reversion(comment):
     '''
     Parse a comment, and determine whether it is a reversion.
     '''
 
+
+ACCOUNT = re.compile('^.*|([^\]]+)\]\]')
 def account(comment):
     '''
     Determine the account whose edits were reverted.
     '''
+    candidates = re.findall(r'\|([^ \]]+)\]', comment)
+    if len(candidates) > 0:
+        b = candidates[0]
+    else:
+        b = None
+    return None, b
